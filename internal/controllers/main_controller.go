69fb43dbc1cbf9e1a5bf1b1a153966856f3f7e1e
Azzamsyakir <azzamsykir@gmail.com>
2024-12-25 10:00:44 +0700

    feat: add ScrapeSoldSellerProducts feature

--- internal/controllers/main_controller.go
+++ internal/controllers/main_controller.go
@@ -45,7 +45,7 @@ func (mainController *MainController) GetAllSellerProducts(c *gin.Context) {
 	defer rabbitMqChannel.Close()
 
 	_, err = rabbitMqChannel.QueueDeclare(
-		"GetSellerProduct Queue",
+		"GetAllSellerProduct Queue",
 		true,
 		false,
 		false,
@@ -61,7 +61,52 @@ func (mainController *MainController) GetAllSellerProducts(c *gin.Context) {
 		return
 	}
 
-	mainController.Producer.CreateMessageGetSellerProducts(rabbitMqChannel, seller)
+	mainController.Producer.CreateMessageGetAllSellerProducts(rabbitMqChannel, seller)
+	responseData := <-mainController.ResponseChannel
+	var zeroResponse Response[map[string]interface{}]
+	if responseData.Code != zeroResponse.Code {
+		c.JSON(responseData.Code, responseData)
+	} else {
+		result := &Response[map[string]interface{}]{
+			Code:    http.StatusBadRequest,
+			Message: "Failed to retrieve products, cannot get response from message rabbitMq",
+		}
+		c.JSON(result.Code, result)
+		return
+	}
+}
+func (mainController *MainController) GetSoldSellerProducts(c *gin.Context) {
+	seller := c.Param("seller")
+	RabbitMQConnection := mainController.Rabbitmq.Connection
+	rabbitMqChannel, err := RabbitMQConnection.Channel()
+	if err != nil {
+		result := &Response[map[string]interface{}]{
+			Code:    http.StatusBadRequest,
+			Message: err.Error(),
+		}
+		c.JSON(result.Code, result)
+		return
+	}
+	defer rabbitMqChannel.Close()
+
+	_, err = rabbitMqChannel.QueueDeclare(
+		"GetSoldSellerProduct Queue",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		result := &Response[map[string]interface{}]{
+			Code:    http.StatusBadRequest,
+			Message: err.Error(),
+		}
+		c.JSON(result.Code, result)
+		return
+	}
+
+	mainController.Producer.CreateMessageGetSoldSellerProducts(rabbitMqChannel, seller)
 	responseData := <-mainController.ResponseChannel
 	var zeroResponse Response[map[string]interface{}]
 	if responseData.Code != zeroResponse.Code {
--- internal/rabbitmq/producer/main_controller.go
+++ internal/rabbitmq/producer/main_controller.go
@@ -14,8 +14,35 @@ func CreateNewMainControllerProducer() *MainControllerProducer {
 	return mainControllerProducer
 }
 
-func (*MainControllerProducer) CreateMessageGetSellerProducts(channelRabbitMQ *amqp.Channel, seller string) error {
-	queueName := "GetSellerProduct Queue"
+func (*MainControllerProducer) CreateMessageGetAllSellerProducts(channelRabbitMQ *amqp.Channel, seller string) error {
+	queueName := "GetAllSellerProduct Queue"
+	payload := map[string]interface{}{
+		"message": "Start Scraping",
+		"seller":  seller,
+		"channel": channelRabbitMQ,
+	}
+	messageBody, err := json.Marshal(payload)
+	if err != nil {
+		return fmt.Errorf("failed to marshal message body: %w", err)
+	}
+	message := amqp.Publishing{
+		ContentType: "text/plain",
+		Body:        []byte(messageBody),
+	}
+	if err := channelRabbitMQ.Publish(
+		"",        // exchange
+		queueName, // queue name
+		false,     // mandatory
+		false,     // immediate
+		message,   // message to publish
+	); err != nil {
+		return fmt.Errorf("failed to publish message to queue: %w", err)
+	}
+	return nil
+}
+
+func (*MainControllerProducer) CreateMessageGetSoldSellerProducts(channelRabbitMQ *amqp.Channel, seller string) error {
+	queueName := "GetSoldSellerProduct Queue"
 	payload := map[string]interface{}{
 		"message": "Start Scraping",
 		"seller":  seller,
