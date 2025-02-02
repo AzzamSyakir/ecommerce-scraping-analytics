bde255f362fb4ae859942a947d1687f043312be5
Azzamsyakir <azzamsykir@gmail.com>
2025-02-02 20:14:05 +0700

    feat: add cors middleware with rs/cors lib

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -12,6 +12,7 @@ func NewMiddleware() *Middleware {
 	c := cors.New(cors.Options{
 		AllowedOrigins:   []string{"*"},
 		AllowCredentials: true,
+		AllowedHeaders:   []string{"*"},
 		// Enable Debugging for testing, consider disabling in production
 		Debug: false,
 	})
